86d6c094815bc7efd47a24dac983a5396fa09017
NguyenVanThanhHust <nguyen.van.thanh.hust@gmail.com>
2020-06-20 15:20:08 +0700

    define Standarad ROI Heads

--- TridentNetwork/tridentnet/trident_rcnn.py
+++ TridentNetwork/tridentnet/trident_rcnn.py
@@ -57,3 +57,29 @@ class TridentRes5ROIHeads(Res5ROIHeads):
 
             return pred_instances, {}
 
+@ROI_HEADS_REGISTRY.register()
+class TridentStandardROIHeads(StandardHOIHeads):
+    def __init__(self, cfg, input_shape):
+        super(TridentStandardROIHeads, self).__init__(cfg, input_shape)
+        
+        self.num_branch = cfg.MODEL.TRIDENT.NUM_BRACNH
+        self.trident_fast = cfg.MODEL.TRIDENT.TEST_BRANCH_IDX != -1
+
+    def forward(self, images, features, proposals, targets=None):
+        """
+        
+        """ 
+        num_branch = self.num_branch if self.training or not self.trident_fast else 1
+        all_targets = targets * num_branch if targets is not None else None
+        pred_instances, losses = super().forward(images, featrues, proposals, all_targets)
+        del images, all_targets, targets
+    
+        if self.training:
+            return pred_instances, losses
+        else:
+            pred_instances = merge_branch_instances(
+                pred_instances,
+                num_branch,
+                self.box_predictor.test_nms_thresh, 
+                self.box_predictor.test_topk_per_image)
+            return pred_instances, {} 
