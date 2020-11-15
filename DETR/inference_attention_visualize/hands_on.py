f9fc0071f21dbf2ae009453b238a20b510da7c08
NguyenVanThanhHust <nguyen.van.thanh.hust@gmail.com>
2020-11-15 19:57:54 +0700

    result visualize + human detection

--- DETR/inference_attention_visualize/hands_on.py
+++ DETR/inference_attention_visualize/hands_on.py
@@ -1,4 +1,4 @@
-from PIL import Image
+from PIL import Image, ImageDraw
 import requests
 import matplotlib.pyplot as plt
 
@@ -16,7 +16,7 @@ model.eval()
 
 # standard PyTorch mean-std input image normalization
 transform = T.Compose([
-    T.Resize(1200),
+    T.Resize(800),
     T.ToTensor(),
     T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
 ])
@@ -37,3 +37,53 @@ keep = probas.max(-1).values > 0.9
 # convert boxes from [0; 1] to image scales
 bboxes_scaled = rescale_bboxes(outputs['pred_boxes'][0, keep], im.size)
 print(bboxes_scaled)
+
+# we want to visulize last decoder layers
+# use lists to store the outputs via up-values
+conv_features, enc_atten_weights, dec_atten_weights = [], [], []
+hooks = [
+    model.backbone[-2].register_forward_hook(
+        lambda self, intput, output: conv_features.append(output)
+    ), 
+    model.transformer.encoder.layers[-1].self_attn.register_forward_hook(
+        lambda self, intput, output: enc_atten_weights.append(output[1])
+    ), 
+    model.transformer.decoder.layers[-1].multihead_attn.register_forward_hook(
+        lambda self, intput, output: dec_atten_weights.append(output[1])
+    ),
+]
+
+# propagate through the model
+outputs = model(img)
+for hook in hooks:
+    hook.remove()
+
+conv_features = conv_features[0]
+enc_atten_weights = enc_atten_weights[0]
+dec_atten_weights = dec_atten_weights[0]
+
+# get the feature map shape
+h, w = conv_features['0'].tensors.shape[-2:]
+print(h, w)
+for idx, (xmin, ymin, xmax, ymax) in enumerate(bboxes_scaled):
+    # create rectangle image 
+    temp_im = im
+    draw_img = ImageDraw.Draw(temp_im)
+    image = dec_atten_weights[0, idx].view(h, w)
+    trans = T.ToPILImage()
+    image = trans(image).convert("RGB")
+    image = image.save("attention_map_" + str(idx) + ".jpg")
+    shape = [(xmin, ymin), (xmax, ymax)]
+    draw_img.rectangle(shape, fill = (255,0,0))
+    temp_im.save("object_bbox" + str(idx) + ".jpg")
+
+# output of the CNN
+f_map = conv_features['0']
+print("Encoder attention:      ", enc_atten_weights[0].shape)
+print("Feature map:            ", f_map.tensors.shape)
+
+# get the HxW shape of the feature maps of the CNN
+shape = f_map.tensors.shape[-2:]
+# and reshape the self-attention to a more interpretable shape
+sattn = enc_atten_weights[0].reshape(shape + shape)
+print("Reshaped self-attention:", sattn.shape)
